06e19b83a2373ab128b2ebaaa575dec1e2875033
aniruddh <71829056+an1ruddh@users.noreply.github.com>
2022-03-13 15:50:01 +0530

    Create stacklist.c

---
+++ stacklist.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
+
+
+struct node{
+	int info ; 
+	struct node *next ; 
+};
+
+struct node *head ; 
+
+
+
+bool isEmpty(){
+	if(head==NULL) return true ; 
+	return false ; 
+}
+void push(int element){
+	struct node *top = (struct node*) 
+	malloc(sizeof(struct node)) ; 
+	if(isEmpty()){ // case 1 
+		printf("Insert Not possible\n") ; 
+	}else if(head == NULL){ // first element case 2 
+		printf("First element\n") ; 
+		top->info = element ; 
+		top->next = NULL ; 
+		head = top ; 
+	}else{ // stack is not empty  case 3 
+		top->info = element ; 
+		top->next = head ; 
+		head = top ; 
+	}
+}
+
+int peek(){
+		if(isEmpty()) return NULL ; 
+		else return head->info; 
+}
+
+void display(){
+	if(isEmpty()) printf("Stack is Empty\n") ;
+	else{
+		top = head ; 
+		while(top!=NULL){
+			printf("Element:%d\n", top->info ); 
+			top = top->next; 
+		}
+	} 
+}
+
+void pop(){
+	int valueDeleted ; 
+	if(isEmpty()){
+		printf("Stack is Empty\n") 
+	}else{
+		top = head ; 
+		valueDeleted = head ->info ; 
+		head = head->next; 
+		free(top) ; 
+	}
+	
+}
+
+int peek(){
+	
+}
+
+void display(){
+	
+}
+
+int main(){
+	return 0 ; 
+}
