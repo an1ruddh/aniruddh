5a45741356e60f69bc487a7923a6d7cda23fec90
aniruddh <71829056+an1ruddh@users.noreply.github.com>
2022-03-14 14:50:39 +0530

    Update doublyll.c

--- doublyll.c
+++ doublyll.c
@@ -7,19 +7,22 @@ struct node
     int data;  
 };  
 struct node *head;  
-
 void insertion_beginning();  
 void insertion_last();  
+void insertion_specified();  
 void deletion_beginning();  
 void deletion_last();  
+void deletion_specified();  
 void display();  
- 
+void search();  
 void main ()  
 {  
 int choice =0;  
-    while(1)  
+    while(choice != 9)  
     {  
-      
+        printf("\n*********Main Menu*********\n");  
+        printf("\nChoose one option from the following list ...\n");  
+        printf("\n===============================================\n");  
         printf("\n1.Insert in begining\n2.Insert at last\n3.Insert at any random location\n4.Delete from Beginning\n  
         5.Delete from last\n6.Delete the node after the given data\n7.Search\n8.Show\n9.Exit\n");  
         printf("\nEnter your choice?\n");  
@@ -30,18 +33,27 @@ int choice =0;
             insertion_beginning();  
             break;  
             case 2:  
-            insertion_last();  
+                    insertion_last();  
             break;  
             case 3:  
+            insertion_specified();  
+            break;  
+            case 4:  
             deletion_beginning();  
             break;  
             case 5:  
             deletion_last();  
             break;  
             case 6:  
-            display();  
+            deletion_specified();  
             break;  
             case 7:  
+            search();  
+            break;  
+            case 8:  
+            display();  
+            break;  
+            case 9:  
             exit(0);  
             break;  
             default:  
@@ -117,7 +129,39 @@ void insertion_last()
        }  
      printf("\nnode inserted\n");  
     }  
-}
+void insertion_specified()  
+{  
+   struct node *ptr,*temp;  
+   int item,loc,i;  
+   ptr = (struct node *)malloc(sizeof(struct node));  
+   if(ptr == NULL)  
+   {  
+       printf("\n OVERFLOW");  
+   }  
+   else  
+   {  
+       temp=head;  
+       printf("Enter the location");  
+       scanf("%d",&loc);  
+       for(i=0;i<loc;i++)  
+       {  
+           temp = temp->next;  
+           if(temp == NULL)  
+           {  
+               printf("\n There are less than %d elements", loc);  
+               return;  
+           }  
+       }  
+       printf("Enter value");  
+       scanf("%d",&item);  
+       ptr->data = item;  
+       ptr->next = temp->next;  
+       ptr -> prev = temp;  
+       temp->next = ptr;  
+       temp->next->prev=ptr;  
+       printf("\nnode inserted\n");  
+   }  
+}  
 void deletion_beginning()  
 {  
     struct node *ptr;  
@@ -166,7 +210,32 @@ void deletion_last()
         printf("\nnode deleted\n");  
     }  
 }  
-
+void deletion_specified()  
+{  
+    struct node *ptr, *temp;  
+    int val;  
+    printf("\n Enter the data after which the node is to be deleted : ");  
+    scanf("%d", &val);  
+    ptr = head;  
+    while(ptr -> data != val)  
+    ptr = ptr -> next;  
+    if(ptr -> next == NULL)  
+    {  
+        printf("\nCan't delete\n");  
+    }  
+    else if(ptr -> next -> next == NULL)  
+    {  
+        ptr ->next = NULL;  
+    }  
+    else  
+    {   
+        temp = ptr -> next;  
+        ptr -> next = temp -> next;  
+        temp -> next -> prev = ptr;  
+        free(temp);  
+        printf("\nnode deleted\n");  
+    }     
+}  
 void display()  
 {  
     struct node *ptr;  
@@ -178,26 +247,38 @@ void display()
         ptr=ptr->next;  
     }  
 }   
-void reverseList()
-{
-    struct node *current, *temp;
-
-    current = head;
-    while(current != NULL)
-    {
-       
-       
-        temp = current->next;
-        current->next = current->prev;
-        current->prev = temp;
- 
-        current = temp;
-    }
-    
-    
-    temp = head;
-    head = last;
-    last = temp;
-
-    printf("LIST REVERSED SUCCESSFULLY.\n");
-}
+void search()  
+{  
+    struct node *ptr;  
+    int item,i=0,flag;  
+    ptr = head;   
+    if(ptr == NULL)  
+    {  
+        printf("\nEmpty List\n");  
+    }  
+    else  
+    {   
+        printf("\nEnter item which you want to search?\n");   
+        scanf("%d",&item);  
+        while (ptr!=NULL)  
+        {  
+            if(ptr->data == item)  
+            {  
+                printf("\nitem found at location %d ",i+1);  
+                flag=0;  
+                break;  
+            }   
+            else  
+            {  
+                flag=1;  
+            }  
+            i++;  
+            ptr = ptr -> next;  
+        }  
+        if(flag==1)  
+        {  
+            printf("\nItem not found\n");  
+        }  
+    }     
+          
+} 
